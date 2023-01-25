a03583ac2580cbd9b073297a5b88c5f3f567899f
Sasdalorian <randomweas1100@gmail.com>
2023-01-25 19:54:34 -0300

    arreglos indextree.js

--- js/indextree.js
+++ js/indextree.js
@@ -6,9 +6,6 @@ document.getElementById('formatoFisica3').style.display = "none";
 document.getElementById('formatoFisica4').style.display = "none";
 document.getElementById('placa').style.display = "none";
 
-const btnparaMi = document.getElementById('button__parami');
-const btnRegalo = document.getElementById('button__regalo');
-
 let parami = document.getElementsByClassName("form__parami");
 let regalo = document.getElementsByClassName("form__regalo");
 
@@ -43,6 +40,7 @@ $("#opcionDigital").click(function() {
     $("#formatoFisica4").hide();
 })
 
+
 document.getElementById("incluirPlaca").addEventListener("change", function(){
     var miParrafo = document.getElementById("placa");
     if(!this.checked){
@@ -51,3 +49,67 @@ document.getElementById("incluirPlaca").addEventListener("change", function(){
         miParrafo.style.display = "block";
     }
 });
+
+
+/* ALERT */
+
+/* Variables Regalo */
+const nombreInputR = document.querySelector("#nombreInputR");
+const apellidoInputR = document.querySelector("#apellidoInputR");
+const emailInputR = document.querySelector("#emailInputR");
+const dirInputR = document.querySelector("#direccionInputR");
+const regionInputR = document.querySelector("#regionInputR");
+const comunaInputR = document.querySelector("#comunaInputR");
+
+/* Variables Mi */
+const nombreInputM = document.querySelector("#nombreInputM");
+const apellidoInputM = document.querySelector("#apellidoInputM");
+const emailInputM = document.querySelector("#emailInputM");
+const rutInputM = document.querySelector("#rutInputM");
+const telInputM = document.querySelector("#telInputM");
+const direccionInputM = document.querySelector("#direccionInputM");
+const regionInputM = document.querySelector("#regionInputM");
+const comunaInputM = document.querySelector("#comunaInputM");
+const mensajeInputT = document.querySelector("#mensajeInputT");
+
+/* Variables Global */
+const formulario = document.querySelector(".form__contenedor");
+const btnEnviar = document.querySelector("#btnEnviar");
+
+
+const datosR = {
+    nombreInputR: '',
+    apellidoInputR: '',
+    emailInputR: '',
+    dirInputR: '',
+    regionInputR: '',
+    comunaInputR: ''
+}
+
+const datosM = {
+    nombreInputM: '',
+    apellidoInputM: '',
+    emailInputM: '',
+    rutInputM: '',
+    telInputM: '',
+    direccionInputM: '',
+    regionInputM: '',
+    comunaInputM: '',
+    mensajeInputT: ''
+}
+
+nombreInputR.addEventListener('blur', leerTexto);
+apellidoInputR.addEventListener('blur', leerTexto);
+emailInputR.addEventListener('blur', leerTexto);
+dirInputR.addEventListener('blur', leerTexto);
+regionInputR.addEventListener('blur', leerTexto);
+comunaInputR.addEventListener('blur', leerTexto);
+
+//Submit
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
+
+function leerTexto(e) {
+    datosM[e.target.id] = e.target.value;
+}
