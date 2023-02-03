ca504c855a2c671cdc70472f1e97b7b48fd6d017
Sasdalorian <randomweas1100@gmail.com>
2023-02-02 21:28:22 -0300

    sweetalert

--- js/login.js
+++ js/login.js
@@ -1,23 +1,25 @@
 const usuarios = [
-    {username: "Sasdalorian", password: "contraseñapaprobar", email: "sasdalorian.contraseña@gmail.com", id: 1},
-    {username: "Letuse", password: "contraseñapaprobar2", email: "lechuga.androgena@gmail.com", id: 2},
-    {username: "Gato", password: "contraseñapaprobar3", email: "gato3000@gmail.com", id: 3},
-    {username: "Cono", password: "contraseñapaprobar4", email: "con0no@gmail.com", id: 4}
+    {nombreUsuario: "Sasdalorian", password: "1234", email: "sasdalorian.contraseña@gmail.com", id: 1},
+    {nombreUsuario: "Letuse", password: "contraseñapaprobar2", email: "lechuga.androgena@gmail.com", id: 2},
+    {nombreUsuario: "Gato", password: "contraseñapaprobar3", email: "gato3000@gmail.com", id: 3},
+    {nombreUsuario: "Cono", password: "contraseñapaprobar4", email: "con0no@gmail.com", id: 4},
+    {nombreUsuario: "OhZeta", password: "contraseñapaprobar5", email: "zzzwaffle@gmail.com", id: 5}
 ]
 
 btnLogin = document.querySelector('#btnLogin');
-btnSingUp = document.querySelector('#SingUp');
+loginModal = document.querySelector('#modal')
 
 btnLogin.addEventListener('click', login)
 
-function login() {
-    const loginUser = document.querySelector('#login-signUp');
-    const passUser = document.querySelector('#SingUp');
+function login(e) {
+    const loginUser = document.querySelector('#login-login');
+    const loginPass = document.querySelector('#password-login');
+    e.preventDefault();
 
-    const buscar = usuarios.filter(username => {
-        if (loginUser.value === usuarios.username || loginUser === usuarios.email && passUser === usuarios.password) {
-            Swal.fire('Any fool can use a computer')
+   usuarios.filter(users => {
+        if(loginUser.value === users.nombreUsuario && loginPass.value === users.password) {
+            Swal.fire('AHORA SI QUE SIPOOOOO.');
+            console.log('pasamo');
         }
     })
-
 }
