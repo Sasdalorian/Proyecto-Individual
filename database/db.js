634172b453aa3a9193976b404de2755ddeffadf3
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-03-29 12:54:36 -0300

    presentacion hecha

--- database/db.js
+++ database/db.js
@@ -11,13 +11,6 @@ conexion.connect(function(error){
     if(error){
         throw error;
     } else {
-        conexion.query('SELECT * FROM tvolun', (error, results) => {
-            if(error) {
-                throw error;
-            } else {
-                console.log('DB connected')
-            }
-        })
         console.log('Conexion Exitosa');
     }
 });
